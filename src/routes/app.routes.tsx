4b37191e3fdfa1053d24a2e5494334e4b8ba7d2a
luccasecco <luccasecco@hotmail.com>
2022-10-06 10:12:44 -0300

    prevents glitchi

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,11 +1,16 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { View } from 'react-native';
 
 import { Home } from '@screens/Home'
 import { NewPhrase } from '@screens/NewPhrase'
+import { useTheme } from 'styled-components';
 const { Navigator, Screen } = createNativeStackNavigator();
 
 export function AppRoutes() {
+  const { COLORS } = useTheme()
+
   return (
+    <View style={{ flex: 1, backgroundColor: COLORS.GRAY_600 }}>
     <Navigator screenOptions={{ headerShown: false }}>
       <Screen 
         name="home"
@@ -16,5 +21,6 @@ export function AppRoutes() {
         component={NewPhrase}
       />
     </Navigator>
+    </View>
   )
 }
